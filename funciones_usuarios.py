76f57294016e46423e15abdebca6c6aece29c39f
Rodrigo Carle <rcarle@uade.edu.ar>
2026-08-25 20:08:24 -0300

    Adaptacion funcion cargar usuarios

--- funciones_usuarios.py
+++ funciones_usuarios.py
@@ -1,3 +1,14 @@
+from listas_usuarios import usuarios
+from funciones_canales import validar_horario
+from menu_seleccion import seleccionar_programa
+
+def validarEdad(edad):
+    if edad < 0:
+        return -1
+    else:
+        return edad
+
+
 def CargarUsuarios(matriz_usuario):
     usuario=[]
     nombre=input("Ingrese el nombre y apellido: ")
@@ -7,26 +18,21 @@ def CargarUsuarios(matriz_usuario):
         edad=int(input("Edad invalida, ingrese la edad nuevamente: "))
         edad=validarEdad(edad)
         
-    mail=input("Ingrese el correo electronico: ")
-    prog=input("Ingrese el programa mas visto: ")
+    prog=seleccionar_programa()
+    streamer=input("Ingrese el streamer favorito: ")
 
-    
+    inicio_horario=input("Ingrese el inicio de su horario recurrente: ")
+    while validar_horario(inicio_horario)==False:
+        inicio_horario=input("Horario invalido, ingrese el inicio de su horario recurrente nuevamente: ")
+    fin_horario=input("Ingrese el fin de su horario recurrente: ")
+    while validar_horario(fin_horario)==False and inicio_horario>=fin_horario:
+        fin_horario=input("Horario invalido, ingrese el fin de su horario recurrente nuevamente: ")
+
+    usuario.append(len(matriz_usuario)+1)
     usuario.append(nombre)
-    usuario.append(edad) #Append de un True (mayor de 16) o False (menor de 16)
-    usuario.append(mail)
+    usuario.append(edad)
     usuario.append(prog)
+    usuario.append(streamer)
+    usuario.append([inicio_horario, fin_horario])
     matriz_usuario.append(usuario)
-
-
-
-    
-
-    
-def validarEdad(edad):
-    if edad < 0:
-        return -1
-
-    if edad < 16:
-        return False
-    else:
-        return True
+    return matriz_usuario
