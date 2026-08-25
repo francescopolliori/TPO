ef4a724fb5a534c1196c4e2ee4d8961a5d30c895
Rodrigo Carle <rcarle@uade.edu.ar>
2026-08-25 16:48:44 -0300

    Creacion lista usuarios base

---
+++ listas_usuarios.py
@@ -0,0 +1,15 @@
+matriz = [
+    [1, "Pepe", 25, 0, 2, ["17:00", "19:00"]],
+    [2, "Ana", 30, 1, 1, ["18:00", "20:00"]],
+    [3, "Luis", 22, 0, 3, ["19:00", "21:00"]],
+
+    ]
+""" Lista de usuarios con sus datos:
+    Cada usuario es una lista con los siguientes elementos:
+    0: ID (entero)
+    1: Nombre (cadena)
+    2: Edad (entero)
+    3: Suscripcion a canal (indice)
+    4: Streamer favorito (indice)
+    5: Horarios recurrentes (lista de cadenas)
+"""
